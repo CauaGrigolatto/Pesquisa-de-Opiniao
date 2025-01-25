e622b38581096e0b9411932e77c178cfd660aa97
Cauã Grigolatto <=>
2025-01-25 14:29:02 -0300

    Add entities

---
+++ app/src/main/java/br/edu/ifsp/dmo/pesquisa/data/model/entity/Student.kt
@@ -0,0 +1,4 @@
+package br.edu.ifsp.dmo.pesquisa.data.model.entity
+
+class Student(val id: String, val fullName: String) {
+}
---
+++ app/src/main/java/br/edu/ifsp/dmo/pesquisa/data/model/entity/Vote.kt
@@ -0,0 +1,6 @@
+package br.edu.ifsp.dmo.pesquisa.data.model.entity
+
+import java.util.UUID
+
+class Vote(val id: UUID, val value: String) {
+}
