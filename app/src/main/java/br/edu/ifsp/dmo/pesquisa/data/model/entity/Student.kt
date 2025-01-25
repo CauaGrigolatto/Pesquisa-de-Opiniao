ee07bd6ee641ddb5cf5afcc2b84ab155b7747351
Cauã Grigolatto <=>
2025-01-25 14:29:53 -0300

    Add voted flag on Student

--- app/src/main/java/br/edu/ifsp/dmo/pesquisa/data/model/entity/Student.kt
+++ app/src/main/java/br/edu/ifsp/dmo/pesquisa/data/model/entity/Student.kt
@@ -1,4 +1,5 @@
 package br.edu.ifsp.dmo.pesquisa.data.model.entity
 
-class Student(val id: String, val fullName: String) {
+class Student(val id: String, val fullName: String, val voted: Boolean = false) {
+
 }
